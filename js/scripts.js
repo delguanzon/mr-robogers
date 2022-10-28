7b80cd0b8cde488d9f05ba77697b3f0aa67dab2d
delguanzon <yodelguanzon@gmail.com>
2022-10-28 12:58:30 -0700

    Add new test and pass
    Replace number 1 with 'Beep!', number 2 with 'Boop!' and number 3 with 'Won't you be my neighbor?' in the resulting array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 function translateCode(code) {
   numbers = []
-  for(i = 0; i <= code; i++){
+  for(i = 0; i <= code; i++) {
+    if(i === 1) {
+      numbers.push('Beep')
+    }
+    if(i === 2) {
+      numbers.push('Boop')
+    }
+    if(i === 3) {
+      numbers.push('Won\'t you be my neighbor?')
+    }
     numbers.push(i);
   }
   return numbers;
