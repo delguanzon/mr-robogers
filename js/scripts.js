73c3c49f7c4e5e9f5879fd07b0c3448cb4fe5e9d
delguanzon <yodelguanzon@gmail.com>
2022-10-31 03:18:32 -0700

    Add and pass test for displaying user's name after code 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
 //Business Logic
 
-function translateCode(code) {  
-    if(code === 1) {
-      return 'Beep';
-    }
-    if(code === 2) {
-      return 'Boop';
-    }
-    if(code === 3) {
-      return 'Won\'t you be my neighbor?';
-    }
-    return code;
+function translateCode(code) {
+  const name = document.getElementById('name').value.trim().split(' ')[0]; 
+  if(code === 1) {
+    return 'Beep';
   }
+  if(code === 2) {
+    return 'Boop';
+  }
+  if(name.length > 0 && code === 3) {
+    return 'Won\'t you be my neighbor, ' + name + '?';
+  }
+  else if(code === 3) {
+    return 'Won\'t you be my neighbor?';
+  }  
+  return code;
+}
 
 function compareDigits(number) {
   const numbers = String(number).split('').map(Number);
@@ -50,13 +54,13 @@ function generateDialog(number) {
 function displayDialog(event, ){
   event.preventDefault();
 
-  const name = document.getElementById("name").value;
-  let p = document.createElement("p");  
-  let responseDiv = document.getElementById("resBox");  
-  let code = parseInt(document.getElementById("code").value);
+  const name = document.getElementById('name').value;
+  let p = document.createElement('p');  
+  let responseDiv = document.getElementById('resBox');  
+  let code = parseInt(document.getElementById('code').value);
 
   generateDialog(code).forEach(function (element) {
-    let span = document.createElement("span");
+    let span = document.createElement('span');
     span.append(element);
     p.append(span);
   });
@@ -66,22 +70,22 @@ function displayDialog(event, ){
 
 function hideForm(event) {
   event.preventDefault();
-  const p = document.getElementById("welcomePg"); 
-  const name = document.getElementById("name").value.trim().split(" ")[0];
-  const welcome = document.getElementById("welcome");
-  const chatBox = document.getElementById("chatBox");
-  welcome.setAttribute("class","hidden");
-  chatBox.setAttribute("class","chatBox");
+  const p = document.getElementById('welcomePg'); 
+  const name = document.getElementById('name').value.trim().split(' ')[0];
+  const welcome = document.getElementById('welcome');
+  const chatBox = document.getElementById('chatBox');
+  welcome.setAttribute('class','hidden');
+  chatBox.setAttribute('class','chatBox');
   if(name.length > 0) {
-    let newName = " " + name + "!";
+    let newName = ' ' + name + '!';
     p.append(newName);
   }
   
 }
 
-window.addEventListener("load", function() {
-  const form = document.getElementById("form");
-  const nameBtn = document.getElementById("nameBtn");
-  nameBtn.addEventListener("click", hideForm);
-  form.addEventListener("submit", displayDialog);
+window.addEventListener('load', function() {
+  const form = document.getElementById('form');
+  const nameBtn = document.getElementById('nameBtn');
+  nameBtn.addEventListener('click', hideForm);
+  form.addEventListener('submit', displayDialog);
 });
